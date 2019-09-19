1162e23d73441bb6c251680eee9fbe63dcd157ee
emilyshapiro <emily@scripted.org>
2019-09-19 13:28:30 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,40 @@
+// do not modify the todays_menu variable
+var todays_menu = {
+    breakfast: "eggs",
+    lunch: "pizza",
+    dinner: {
+        first_course:"salad",
+        second_course: "tomato soup",
+        final_course: "steak/vegan steak"
+    },
+    dessert_options: [
+     "flourless chocolate cake",
+     "apple pie",
+     "strawberry milkshake"
+    ]
+};
+// do not modify the todays_menu variable
+
+
+// 1)
+
+
+// 2)
+console.log(todays_menu["breakfast"]);
+
+// 3)
+console.log(todays_menu.dinner.second_course = "chicken noodle soup");
+
+// 4)
+
+console.log(todays_menu["dessert_options"][0]);
+// 5)
+console.log(todays_menu["dessert_options"][2]= "blueberry coffee cake");
+
+// 6)
+console.log(todays_menu.dinner.final_course);
+
+
+
+
+
